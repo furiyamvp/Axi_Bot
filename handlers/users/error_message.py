4c7aa1d71e7b74bc143793f6fb3f551b1a3fc258
furiyamvp <rixsiboyevyusuf87@gmail.com>
2024-07-16 23:22:15 +0500

    error message

--- handlers/users/error_message.py
+++ handlers/users/error_message.py
@@ -3,7 +3,7 @@ from loader import dp
 
 
 @dp.message_handler()
-async def error_message(message: types.Message):
+async def error_message_text_handler(message: types.Message):
     if message.text.isdigit():
         text = "Kino kodi 3 xonli son bo'ladi"
         await message.answer(text=text)
@@ -15,3 +15,14 @@ async def error_message(message: types.Message):
             "qachon 3 xonali boladi"
         )
         await message.answer(text=text)
+
+
+@dp.message_handler(content_types=types.ContentType.ANY)
+async def error_message_all_handler(message: types.Message):
+    text = (
+        "Iltimos botga xar qanday narsalar tashlamang, Bot orqali siz bizni kinolarimni korishingiz mumkin "
+        "kino topish uchun bizni instagram kanalimizdagi yokida tiktokdagi kanalimizdagi kinoni videoni linkini "
+        "olib botga tashlasangiz kino chiqib keladi, kinoni kodi bilan ham topsangiz boladi, kino kodi har "
+        "qachon 3 xonali boladi"
+    )
+    await message.answer(text=text)
