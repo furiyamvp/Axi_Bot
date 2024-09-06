22b60ea35c3368f7a4d71645d9dbbde1bb2d53bc
furiyamvp <rixsiboyevyusuf87@gmail.com>
2024-09-06 23:23:30 +0500

    reklama avaz

--- main/config.py
+++ main/config.py
@@ -8,7 +8,8 @@ ADMINS = env.list("ADMINS")
 
 CHANNELS = [
     ("https://t.me/Zangoriekran_kanali", -1002107248547, "Zangori Ekran"),
-    ("https://t.me/Yummy_multik", -1001652312277, "Yummy")
+    ("https://t.me/Yummy_multik", -1001652312277, "Yummy"),
+    ("https://t.me/codewithavaz_py", -1002246062103, "CodeWithAvaz")
 ]
 
 DB_USER = env.str("DB_USER")
