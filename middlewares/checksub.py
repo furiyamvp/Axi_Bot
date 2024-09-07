209c5eb22e6eb0d33bf8f710ae98ec56e41d9fe5
furiyamvp <rixsiboyevyusuf87@gmail.com>
2024-09-07 22:58:48 +0500

    admin has not check_subs

--- middlewares/checksub.py
+++ middlewares/checksub.py
@@ -9,26 +9,33 @@ from utils.misc.subscription import check
 
 class BigBrother(BaseMiddleware):
     async def on_pre_process_update(self, update: types.Update, data: dict):
+        user_id = None
+
         if update.message:
-            user = update.message.from_user.id
+            user_id = update.message.from_user.id
         elif update.callback_query:
-            user = update.callback_query.from_user.id
-            if update.callback_query.data == "check_subs" or str(user) in ADMINS:
+            user_id = update.callback_query.from_user.id
+            if update.callback_query.data == "check_subs":
                 return
         else:
             return
 
+        # Skip the check if the user is an admin
+        if str(user_id) in ADMINS:
+            return
+
         final_status = True
         markup = InlineKeyboardMarkup(row_width=1)
+
         for idx, channel in enumerate(CHANNELS, start=1):
-            status = await check(user_id=user, channel=channel[1])
+            status = await check(user_id=user_id, channel=channel[1])
             if not status:
                 final_status = False
                 button = InlineKeyboardButton(text=f"{idx} - kanal", url=channel[0])
                 markup.add(button)
 
         if not final_status:
-            markup.add(InlineKeyboardButton(text="Obunani tekshirish ⭕️\n", callback_data="check_subs"))
+            markup.add(InlineKeyboardButton(text="Obunani tekshirish ⭕️", callback_data="check_subs"))
             await update.message.answer(
                 "Botdan foydalanish uchun quyidagi kanallarga obuna bo'ling ❌",
                 reply_markup=markup
