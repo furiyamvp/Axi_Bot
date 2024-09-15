9b08fa8007f80b4090ba9aeefe3f2b4ae30ae358
furiyamvp <rixsiboyevyusuf87@gmail.com>
2024-09-15 16:47:16 +0500

    finnaly commit

--- handlers/users/users_panel/start.py
+++ handlers/users/users_panel/start.py
@@ -7,5 +7,5 @@ from loader import dp
 
 @dp.message_handler(CommandStart(), state="*")
 async def admin_start_handler(message: types.Message):
-    text = "Botizmiga xush kelibsiz 🫡"
+    text = "Botizmiga xush kelibsiz🫡"
     await message.answer(text=text, reply_markup=await user_main_menu_def())
